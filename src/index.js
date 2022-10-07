f915610454253631322ce4fd7d70c3bc72735284
viktoriia-tsytsura <sholomviktoriia@gmail.com>
2022-10-07 17:22:45 +0200

    fix js script

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './js/anchor-scroll';
 import './js/header-scroll';
+import './js/slider';
 import './js/mobile-menu';
 import './js/modal';
 import './js/simple-lightbox';
-import './js/slick';
-import './js/slider';
+// import './js/slick';
+
