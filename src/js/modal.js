044feea2b6cedf94305a4ea00fd0e6d8817b6f9a
Tsylepa <tsylepa@gmail.com>
2022-10-10 23:43:59 +0100

    Header modal fix

--- src/js/modal.js
+++ src/js/modal.js
@@ -35,3 +35,12 @@ window.onclick = function (event) {
     }
   }
 };
+
+$(document).on('click', '#close_vid', function () {
+  jQuery('iframe').each(function () {
+    jQuery(this)[0].contentWindow.postMessage(
+      '{"event":"command","func":"pauseVideo","args":""}',
+      '*'
+    );
+  });
+});
