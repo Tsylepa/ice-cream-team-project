3586643e1a932b2a9bc88503423ca7b26f9a7b36
ksenofontovaaa <108142737+ksenofontovaaa@users.noreply.github.com>
2022-10-11 21:32:04 +0300

    Merge remote-tracking branch 'origin' into product

--- src/js/modal.js
+++ src/js/modal.js
@@ -18,20 +18,10 @@ for (var i = 0; i < btn.length; i++) {
 
 // When the user clicks on <span> (x), close the modal
 for (var i = 0; i < close.length; i++) {
-  close[i].onclick = function () {
+  close[i].onclick = function (e) {
     for (var index in modals) {
       if (typeof modals[index].style !== 'undefined')
         modals[index].classList.add('is-hidden');
     }
   };
 }
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target.classList.contains('backdrop')) {
-    for (var index in modals) {
-      if (typeof modals[index].style !== 'undefined')
-        modals[index].classList.add('is-hidden');
-    }
-  }
-};
