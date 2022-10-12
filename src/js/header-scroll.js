9087fc14dcbcad283c17ba603e75767d3f5834ed
viktoriia-tsytsura <sholomviktoriia@gmail.com>
2022-10-12 14:31:36 +0200

    fix positioning

--- src/js/header-scroll.js
+++ src/js/header-scroll.js
@@ -1,16 +1,16 @@
-import $ from 'jquery';
+// import $ from 'jquery';
 
-window.onscroll = function () {
-  if (window.screen.width >= 768) {
-    scrollY <= 45
-      ? document.getElementById('header').classList.remove('header-scroll')
-      : document.getElementById('header').classList.add('header-scroll');
-  } else {
-    scrollY <= 120
-      ? document.getElementById('header').classList.remove('header-scroll')
-      : document.getElementById('header').classList.add('header-scroll');
-  }
-};
+// window.onscroll = function () {
+//   if (window.screen.width >= 768) {
+//     scrollY <= 45
+//       ? document.getElementById('header').classList.remove('header-scroll')
+//       : document.getElementById('header').classList.add('header-scroll');
+//   } else {
+//     scrollY <= 120
+//       ? document.getElementById('header').classList.remove('header-scroll')
+//       : document.getElementById('header').classList.add('header-scroll');
+//   }
+// };
 
 // 1. В терміналі прописати команду "npm i jquery"
 // 2. Створити у папці js окремий файл, наприклад, "header-scroll.js" і вставити в нього цей код
